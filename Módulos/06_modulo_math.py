e66e24cd32f930f0261f6c262afdb2365623c59e
Tania <a.klozevits@gmail.com>
2024-03-31 15:24:20 -0600

    definiciones

--- Módulos/06_modulo_math.py
+++ Módulos/06_modulo_math.py
@@ -1,5 +1,5 @@
 from math import e, exp, log
 
-print(pow(e, 1) == exp(log(e)))
+print(pow(e, 1) == exp(log(e)))  #encuentra el valor de xy (toma en cuenta los dominios)
 print(pow(2, 2) == exp(2 * log(2)))
 print(log(e, e) == exp(0))
