32330ba3e1c3b7a401610c3e40a46ecda293be49
Tania <a.klozevits@gmail.com>
2024-04-12 20:21:40 -0600

    commit

--- Módulos/13_modulo_machine.py
+++ Módulos/13_modulo_machine.py
@@ -1,3 +1,10 @@
 from platform import machine
 
 print(machine())
+
+
+#En mi caso, tengo un AMD64
+#AMD64 es un procesador de 64 bits que agrega capacidades informáticas de 64 bits al x86 arquitectura. 
+#AMD64 admite más memoria virtual y física que su contraparte de 32 bits, lo que permite que los programas 
+#almacenen más datos en la memoria. 
+#Otros nombres comunes para AMD64 son x64, x86-64 e Intel 64.
--- Módulos/15_modulo_implementaton_tuple.py
+++ Módulos/15_modulo_implementaton_tuple.py
@@ -4,3 +4,8 @@ print(python_implementation())
 
 for atr in python_version_tuple():
     print(atr)
+
+#Cadena de implementación de Python
+#La parte mayor de la versión de Python.
+#La parte menor.
+#El número del nivel de parche.
