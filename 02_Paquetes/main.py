897c258fb095cdfc3c8dd3d3fcc01a114a615f23
Tania <a.klozevits@gmail.com>
2024-04-19 10:36:33 -0600

    commit

--- 02_Paquetes/main.py
+++ 02_Paquetes/main.py
@@ -1,3 +1,15 @@
 #Contiene el código que utiliza el nuevo módulo.
 
 import module
+
+#Cuando Python importa un módulo por primera vez, traduce el contenido a una forma algo compilada.
+#El archivo no contiene código en lenguaje máquina: 
+# es código semi-compilado interno de Pythoon, listo para ser ejecutado por el interprete de Python.
+
+#El archivo no requiere tantas comprobaciones como las de un archivo fuente,
+#la ejecución comienza más rapido y también se ejecuta más rápido.
+
+# De esta manera, cada importación posterior será más rápida que interpretar
+# que el código fuente desde cero.
+
+
---
+++ 02_Paquetes_ejercicios/02_main.py
@@ -0,0 +1,4 @@
+import module 
+
+#Cuando un módulo es importado, su contenido se ejecuta implícitamente por Python.
+# En este caso, como module.py ya tiene contenido, se imprime directamente en consola al ejecutar este import :)
